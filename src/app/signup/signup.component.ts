054cdbd109b0195f14ce8c944be5e7753f85b4cb
Saranyaullas1999 <saranyaullas1999@gmail.com>
2022-10-15 00:28:25 +0530

    form

--- src/app/signup/signup.component.ts
+++ src/app/signup/signup.component.ts
@@ -9,6 +9,40 @@ export class SignupComponent implements OnInit {
 
   constructor() { }
 
+  
+  name=""
+  phone=""
+  gender=""
+  email=""
+  address=""
+  pincode=""
+  bloodgroup=""
+  dob=""
+  age=""
+  parentName=""
+  pass=""
+  confirmPass=""
+
+  readValue=()=>{
+    let data={
+    "name":this.name,
+  "phone":this.phone,
+  "gender":this.gender,
+  "email":this.email,
+  "address":this.address,
+  "pincode":this.pincode,
+  "bloodgroup":this.bloodgroup,
+  "dob":this.dob,
+  "age":this.age,
+  "parentName":this.parentName,
+  "pass":this.pass,
+  "confirmPass":this.confirmPass
+  }
+  console.log(data)
+  alert("Successfully added")
+}
+  
+
   ngOnInit(): void {
   }
 
